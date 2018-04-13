c644434f2e120e89788c138868bac7ed7241a5f2
Markus Kohlhase <mail@markus-kohlhase.de>
2018-04-13 11:25:29 +0200

    small refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,19 @@ extern crate tokio_core;
 extern crate tokio_modbus;
 extern crate ur20;
 
-use std::{cell::RefCell, collections::HashMap, io::{Error, ErrorKind}, net::SocketAddr};
 use futures::future::{self, Future};
-use tokio_modbus::*;
+use std::{cell::RefCell,
+          collections::HashMap,
+          io::{Error, ErrorKind},
+          net::SocketAddr};
 use tokio_core::reactor::Handle;
-use ur20::{ModuleType, ur20_fbc_mod_tcp::*, ur20_fbc_mod_tcp::Coupler as MbCoupler};
+use tokio_modbus::*;
+use ur20::{ur20_fbc_mod_tcp::Coupler as MbCoupler, ur20_fbc_mod_tcp::*, ModuleType};
 
 pub use ur20::{Address, ChannelValue};
 
 pub struct Coupler {
+    pub id: String,
     client: Client,
     input_count: u16,
     output_count: u16,
@@ -25,112 +29,57 @@ impl Coupler {
     pub fn connect(addr: SocketAddr, handle: Handle) -> impl Future<Item = Coupler, Error = Error> {
         let coupler = Client::connect_tcp(&addr, &handle)
             .and_then(|client| {
-                debug!("Read the coupler ID");
-                client
-                    .read_input_registers(ADDR_COUPLER_ID, 7)
-                    .and_then(move |buff| {
-                        let buf: Vec<u8> = buff.iter().fold(vec![], |mut x, elem| {
-                            x.push((elem & 0xff) as u8);
-                            x.push((elem >> 8) as u8);
-                            x
-                        });
-                        let id = String::from_utf8_lossy(&buf);
-                        info!("Connected to coupler '{}'", id);
-                        Ok(client)
-                    })
-            })
-            .and_then(|client| {
-                debug!("Read module count");
-                client
-                    .read_input_registers(ADDR_CURRENT_MODULE_COUNT, 1)
-                    .and_then(move |buff| {
-                        if buff.is_empty() {
-                            return Err(Error::new(ErrorKind::Other, "Invalid buffer length"));
-                        }
-                        let cnt = buff[0];
-                        if cnt == 0 {
-                            warn!("UR20-System has no modules!");
-                        }
-                        Ok((client, cnt))
-                    })
-            })
-            .and_then(|(client, cnt)| {
-                if cnt == 0 {
-                    // TODO
-                }
-                debug!("Read module list");
-                client
-                    .read_input_registers(ADDR_CURRENT_MODULE_LIST, cnt * 2)
-                    .and_then(move |raw_list| {
-                        let module_list = module_list_from_registers(&raw_list)
-                            .map_err(|err| Error::new(ErrorKind::Other, err))?;
-                        let module_names: Vec<_> = module_list
-                            .iter()
-                            .map(|m| format!("{:?}", m))
-                            .map(|n| n.replace("UR20_", ""))
-                            .collect();
-                        info!("The following I/O modules were detected:");
-                        for (i, n) in module_names.iter().enumerate() {
-                            info!(" {} - {}", i, n);
-                        }
-                        Ok((client, module_list))
-                    })
-            })
-            .and_then(|(client, module_list)| {
-                debug!("read module offsets");
-                client
-                    .read_input_registers(ADDR_MODULE_OFFSETS, module_list.len() as u16 * 2)
-                    .and_then(move |raw_offsets| Ok((client, module_list, raw_offsets)))
-            })
-            .and_then(|(client, module_list, offsets)| {
-                debug!("read process input length");
-                client
-                    .read_input_registers(ADDR_PROCESS_INPUT_LEN, 1)
-                    .and_then(move |raw_input_count| {
-                        let cnt = if raw_input_count.is_empty() {
-                            0
-                        } else {
-                            raw_input_count[0]
-                        };
-                        let input_count = process_img_len_to_register_count(cnt);
-                        Ok((client, module_list, offsets, input_count))
-                    })
-            })
-            .and_then(|(client, module_list, offsets, input_count)| {
-                debug!("read process output length");
-                client
-                    .read_input_registers(ADDR_PROCESS_OUTPUT_LEN, 1)
-                    .and_then(move |raw_output_count| {
-                        let cnt = if raw_output_count.is_empty() {
-                            0
-                        } else {
-                            raw_output_count[0]
-                        };
-                        let output_count = process_img_len_to_register_count(cnt);
-                        Ok((client, module_list, offsets, input_count, output_count))
-                    })
+                let coupler_id = read_coupler_id(&client);
+                let cnt = read_module_count(&client);
+
+                coupler_id.join(cnt).and_then(|(id, cnt)| {
+                    read_module_list(&client, cnt)
+                        .and_then(|module_list| {
+                            print_module_list(&module_list);
+                            Ok((client, id, module_list))
+                        })
+                        .and_then(|(client, id, module_list)| {
+                            read_module_offsets(&client, &module_list)
+                                .and_then(|raw_offsets| Ok((client, id, module_list, raw_offsets)))
+                        })
+                        .and_then(|(client, id, module_list, offsets)| {
+                            read_process_input_register_count(&client).and_then(|input_count| {
+                                Ok((client, id, module_list, offsets, input_count))
+                            })
+                        })
+                        .and_then(|(client, id, module_list, offsets, input_count)| {
+                            read_process_output_register_count(&client).and_then(
+                                move |output_count| {
+                                    Ok((
+                                        client,
+                                        id,
+                                        module_list,
+                                        offsets,
+                                        input_count,
+                                        output_count,
+                                    ))
+                                },
+                            )
+                        })
+                        .and_then(
+                            |(client, id, module_list, offsets, input_count, output_count)| {
+                                read_parameters(&client, &module_list).and_then(move |params| {
+                                    Ok((
+                                        client,
+                                        id,
+                                        module_list,
+                                        offsets,
+                                        input_count,
+                                        output_count,
+                                        params,
+                                    ))
+                                })
+                            },
+                        )
+                })
             })
             .and_then(
-                |(client, module_list, offsets, input_count, output_count)| {
-                    debug!("read parameters");
-                    let params: Vec<_> = param_addresses_and_register_counts(&module_list)
-                        .into_iter()
-                        .map(|(addr, reg_cnt)| client.read_holding_registers(addr, reg_cnt))
-                        .collect();
-                    futures::future::join_all(params).and_then(move |params| {
-                        Ok((
-                            client,
-                            module_list,
-                            offsets,
-                            input_count,
-                            output_count,
-                            params,
-                        ))
-                    })
-                },
-            )
-            .and_then(
-                |(client, modules, offsets, input_count, output_count, params)| {
+                |(client, id, modules, offsets, input_count, output_count, params)| {
                     debug!("create coupler");
                     let cfg = CouplerConfig {
                         modules: modules.clone(),
@@ -140,6 +89,7 @@ impl Coupler {
                     let coupler =
                         MbCoupler::new(&cfg).map_err(|err| Error::new(ErrorKind::Other, err))?;
                     Ok(Coupler {
+                        id,
                         client,
                         coupler: RefCell::new(coupler),
                         input_count,
@@ -229,6 +179,117 @@ impl Coupler {
             })
         })
     }
+}
+
+fn read_coupler_id(client: &Client) -> impl Future<Item = String, Error = Error> {
+    debug!("Read the coupler ID");
+    client
+        .read_input_registers(ADDR_COUPLER_ID, 7)
+        .and_then(move |buff| {
+            let buf: Vec<u8> = buff.iter().fold(vec![], |mut x, elem| {
+                x.push((elem & 0xff) as u8);
+                x.push((elem >> 8) as u8);
+                x
+            });
+            let id = String::from_utf8_lossy(&buf).to_string();
+            Ok(id)
+        })
+}
+
+fn read_module_count(client: &Client) -> impl Future<Item = u16, Error = Error> {
+    debug!("Read module count");
+    client
+        .read_input_registers(ADDR_CURRENT_MODULE_COUNT, 1)
+        .and_then(move |buff| {
+            if buff.is_empty() {
+                return Err(Error::new(ErrorKind::Other, "Invalid buffer length"));
+            }
+            let cnt = buff[0];
+            if cnt == 0 {
+                warn!("UR20-System has no modules!");
+            }
+            Ok(cnt)
+        })
+}
+
+fn read_module_list(
+    client: &Client,
+    cnt: u16,
+) -> impl Future<Item = Vec<ModuleType>, Error = Error> {
+    if cnt == 0 {
+        // TODO
+    }
+    client
+        .read_input_registers(ADDR_CURRENT_MODULE_LIST, cnt * 2)
+        .and_then(move |raw_list| {
+            let module_list = module_list_from_registers(&raw_list)
+                .map_err(|err| Error::new(ErrorKind::Other, err))?;
+            Ok(module_list)
+        })
+}
+
+fn print_module_list(module_list: &[ModuleType]) {
+    let module_names: Vec<_> = module_list
+        .iter()
+        .map(|m| format!("{:?}", m))
+        .map(|n| n.replace("UR20_", ""))
+        .collect();
+    info!("The following I/O modules were detected:");
+    for (i, n) in module_names.iter().enumerate() {
+        info!(" {} - {}", i, n);
+    }
+}
+
+fn read_module_offsets(
+    client: &Client,
+    module_list: &[ModuleType],
+) -> impl Future<Item = Vec<u16>, Error = Error> {
+    debug!("read module offsets");
+    client
+        .read_input_registers(ADDR_MODULE_OFFSETS, module_list.len() as u16 * 2)
+        .and_then(move |raw_offsets| Ok(raw_offsets))
+}
+
+fn read_process_input_register_count(client: &Client) -> impl Future<Item = u16, Error = Error> {
+    debug!("read process input length");
+    client
+        .read_input_registers(ADDR_PROCESS_INPUT_LEN, 1)
+        .and_then(move |raw_input_count| {
+            let cnt = if raw_input_count.is_empty() {
+                0
+            } else {
+                raw_input_count[0]
+            };
+            let input_count = process_img_len_to_register_count(cnt);
+            Ok(input_count)
+        })
+}
+
+fn read_process_output_register_count(client: &Client) -> impl Future<Item = u16, Error = Error> {
+    debug!("read process output length");
+    client
+        .read_input_registers(ADDR_PROCESS_OUTPUT_LEN, 1)
+        .and_then(move |raw_output_count| {
+            let cnt = if raw_output_count.is_empty() {
+                0
+            } else {
+                raw_output_count[0]
+            };
+            let output_count = process_img_len_to_register_count(cnt);
+            Ok(output_count)
+        })
+}
+
+fn read_parameters(
+    client: &Client,
+    module_list: &[ModuleType],
+) -> impl Future<Item = Vec<Vec<u16>>, Error = Error> {
+    debug!("read parameters");
+    let params: Vec<_> = param_addresses_and_register_counts(&module_list)
+        .into_iter()
+        .map(|(addr, reg_cnt)| client.read_holding_registers(addr, reg_cnt))
+        .collect();
+    futures::future::join_all(params).and_then(move |params| Ok(params))
 }
 
 fn process_img_len_to_register_count(process_len: u16) -> u16 {
